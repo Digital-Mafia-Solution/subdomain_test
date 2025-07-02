daa6b5fc0b39258d44c27ad8874afae35eace77e
Siphamandla Mazibuko <100807070+fawkesdbs@users.noreply.github.com>
2025-07-02 22:55:46 +0200

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,12 @@
+const square = document.getElementsByClassName("square")[0];
+const toggle = document.getElementById("toggle");
 
+toggle.addEventListener("click", () => {
+  if (square.classList.contains("red")) {
+    square.classList.remove("red");
+    square.classList.add("blue");
+  } else if (square.classList.contains("blue")) {
+    square.classList.remove("blue");
+    square.classList.add("red");
+  }
+})
